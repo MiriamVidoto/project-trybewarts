d366426092757b6c570499763b7832ff90fd9737
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-05-09 18:44:44 -0300

    requisito 18 ok

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const loginEmail = document.getElementById('email');
 const loginSenha = document.getElementById('senha');
+const check = document.getElementById('agreement');
+const buttonEnviar = document.getElementById('submit-btn');
 
 function login() {
   if (loginEmail.value === 'tryber@teste.com' && loginSenha.value === '123456') {
@@ -9,3 +11,15 @@ function login() {
   }
 }
 document.getElementById('entrar').addEventListener('click', login);
+
+// Verificar se a checkbox do requisito 16 está selecionada
+// referência https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input/checkbox
+// Habilitar o botão entrar
+// referência https://www.w3schools.com/jsref/prop_pushbutton_disabled.asp
+buttonEnviar.disabled = true;
+function enableButton() {
+  if (check.checked) {
+    buttonEnviar.disabled = false;
+  }
+}
+check.addEventListener('click', enableButton);
